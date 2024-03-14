5120cf35940f0180ed98241769dee842e6c36079
Гладков Егор <gladkovyegor@gmail.com>
2024-03-14 15:07:07 +0400

    Update BinarySearchTests.cs
    [Test] -> [TestCase]

--- BinarySearch/BinarySearch.Tests/BinarySearchTests.cs
+++ BinarySearch/BinarySearch.Tests/BinarySearchTests.cs
@@ -1,63 +1,40 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 
 namespace BinarySearch.Tests
 {
     public class BinarySearchTests
     {
-        [Test]
-        public void BinarySearch_SearchLastElement_ReturnsLastIndex()
+        private const int N = 1_000_000_000;
+        private int[] _sortedArray;
+        private int[] _unsortedArray;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
-            // Arrange
-            int[] array = { 2, 4, 6, 8, 10 };
-            int element = 10;
-
-            // Act
-            int result = BinarySearch.Search(array, element);
-
-            // Assert
-            Assert.That(result, Is.EqualTo(4));
-        }
-    
-        [Test]
-        public void BinarySearch_SearchFirstElement_ReturnsIndexZero()
-        {
-            // Arrange
-            int[] array = { 12, 15, 18, 21, 24 };
-            int element = 12;
-
-            // Act
-            int result = BinarySearch.Search(array, element);
-
-            // Assert
-            Assert.That(result, Is.EqualTo(0));
+            this._sortedArray = Enumerable.Range(0, N).ToArray();
+            this._unsortedArray = new int [N];
+            var rand = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _unsortedArray[i] = rand.Next(); 
+            }
         }
 
-        [Test]
-        public void BinarySearch_ElementDoesNotExist_ReturnsNegativeOne()
+        [TestCase(0, ExpectedResult = 0, TestName = "Поиск элемента на первом месте в массиве")]
+        [TestCase(N - 1, ExpectedResult = N - 1, TestName = "Поиск элемента на последнем месте в массиве.")]
+        [TestCase(N / 2, ExpectedResult = N / 2, TestName = "Поиск элемента в середине массива.")]
+        [TestCase(N, ExpectedResult = -1, TestName = "Поиск элемента, которого нет в массиве.")]
+        public int BinarySearch_SortedArray_FindX(int x)
         {
-            // Arrange
-            int[] array = { 2, 4, 6, 8, 10 };
-            int element = 7;
-
-            // Act
-            int result = BinarySearch.Search(array, element);
-
-            // Assert
-            Assert.That(result, Is.Negative);
+            return BinarySearch.Search(_sortedArray, x);
         }
 
-        [Test]
-        public void BinarySearch_ElementExists_ReturnsIndex()
+        [TestCase(-1, ExpectedResult = -1, TestName = "Поиск элемента в неотсортированном массиве.")]
+        public int BinarySearch_UnsortedArray_FindX(int x)
         {
-            // Arrange
-            int[] array = { 1, 3, 5, 7, 9 };
-            int element = 5;
-
-            // Act
-            int result = BinarySearch.Search(array, element);
-
-            // Assert
-            Assert.That(result, Is.EqualTo(2));
+            return BinarySearch.Search(_unsortedArray, x);
         }
     }
 }
